dfb701e2f20018d7d26b8276c325d02d6ae485c8
Jatin <githubgit.repositories@gmail.com>
2023-09-30 17:49:40 +0530

    react-query added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import "../app/globals.css";
 import Home from "./pages/Home";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
