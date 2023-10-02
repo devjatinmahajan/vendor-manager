627eb0b68870ed687279ab586beb09589824da93
Jatin <githubgit.repositories@gmail.com>
2023-10-02 12:03:48 +0530

    Tested out the Delete op

--- frontend/src/components/vendors/vendors-table.tsx
+++ frontend/src/components/vendors/vendors-table.tsx
@@ -1,11 +1,24 @@
-import dummyVendors from "@/data/vendor-details";
+import React from "react";
+import dummyVendors, { Vendor } from "@/data/vendor-details";
 import { DataTable } from "./data-table";
 import { columns } from "./vendor-col";
+import { ColumnDef } from "@tanstack/react-table";
 
-export default function vendorsTable() {
+export default function VendorsTable() {
+  const [vendors, setVendors] = React.useState(dummyVendors);
+  const handleDeleteVendor = (vendorId: string) => {
+    console.log(vendorId);
+    // Filter out the vendor with the specified ID
+    const updatedVendors = vendors.filter((vendor) => vendor.id !== vendorId);
+    console.log(updatedVendors);
+    setVendors(updatedVendors);
+  };
+  const columnsTemp: ColumnDef<Vendor>[] = columns({
+    onDeleteVendor: handleDeleteVendor,
+  });
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={dummyVendors} />
+      <DataTable columns={columnsTemp} data={vendors} />
     </div>
   );
 }
