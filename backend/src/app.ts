5f7fc4371b9adf94a179fb4a8b11b1a46b08b631
Jatin <githubgit.repositories@gmail.com>
2023-10-03 22:31:01 +0530

    production env ready

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
 app.use("/api", vendorRouter);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(path.resolve(), "/frontend/index.html")));
+    app.use(express.static(path.join(path.resolve(), "/frontend/dist")));
     app.get("*", (req, res, next) => {
-        res.sendFile(path.join(path.resolve(), "/frontend/index.html"));
+        res.sendFile(path.join(path.resolve(), "/frontend/dist/index.html"));
     });
 }
 
