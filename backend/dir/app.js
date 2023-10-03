254e3169fd2ca91da2e4f0f89b31d261e76e2f65
Jatin <githubgit.repositories@gmail.com>
2023-10-03 19:55:26 +0530

    tsccommand executed

--- backend/dir/app.js
+++ backend/dir/app.js
@@ -12,16 +12,22 @@ const vendorRoutes_1 = __importDefault(require("./routes/vendorRoutes"));
 dotenv_1.default.config({ path: path_1.default.join(__dirname, "../", ".env") });
 app.use(express_1.default.json());
 app.use((req, res, next) => {
-    console.log("In", process.env.MONGO_USER);
+    console.log("Request is made");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 app.use("/api", vendorRoutes_1.default);
-app.listen(8000);
+if (process.env.NODE_ENV === "production") {
+    app.use(express_1.default.static(path_1.default.join(path_1.default.resolve(), "/frontend/build")));
+    app.get("*", (req, res, next) => {
+        res.sendFile(path_1.default.join(path_1.default.resolve(), "/frontend/build/index.html"));
+    });
+}
+app.listen(process.env.PORT || 8080);
 mongoose
     .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9s9ex2a.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`)
     .then(() => {
-        const server = app.listen(process.env.PORT || 8080);
-    });
+    const server = app.listen(process.env.PORT || 8080);
+});
